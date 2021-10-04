f4d4f35ed4f79e429b51400747ff1819f853a263
yolandalillo <43533573+yolandalillo@users.noreply.github.com>
2021-10-04 16:43:49 +0200

    :running:

---
+++ L3/variables4.s.asm
@@ -0,0 +1,16 @@
+#--Escribe un programa (variables4.s) que tenga un segmento de datos con 4 variables: v1, v2, v3 y v4, 
+#-- inicializadas con los valores 0x12345678, 0x11223344, 0xCACABACA, 0x00FABADA respectivamente. 
+#-- En el segmento de código sólo deben estar las instrucciones para terminar
+
+.data
+
+v1: .word 0x12345678
+v2: .word 0x11223344
+v3: .word 0xCACABACA
+v4: .word 0x00FABADA
+.text
+
+ li a7, 10
+ ecall
+
+#-- Para el archivo binario descargamos .data y binary no binary text

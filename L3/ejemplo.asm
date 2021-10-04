0acf0d69112ef797aaf0f096a0234824a7417c8d
yolandalillo <43533573+yolandalillo@users.noreply.github.com>
2021-10-04 12:26:32 +0200

    :construction:

---
+++ L3/ejemplo.asm
@@ -0,0 +1,10 @@
+#-- Probando asignación
+
+.text
+
+ #-- x5 = 30
+ li x5,30 #-- Es lo mismo que addi x5,x0,30
+ #-- x10 = x5
+ mv x10, x5 #-- Es lo mismo que add x10,x0,x5
+ 
+ 
---
+++ L3/ejemplo2.asm
@@ -0,0 +1,9 @@
+#-- Mi primera variable
+
+.data
+
+.word 0xBEBECAFE #-- Si lo ponemos separado mejor para comentarlo
+.word 0xCAFEBACA
+.word 0xCAFECACA
+.word 1
+.word 2
